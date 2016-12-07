81c6919f1da67cab250275de9759bcf4afb9f934
antonio-peyrano <antonio.peyrano@live.com.mx>
2016-12-07 13:43:40 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Micrositio-Phoenix v1.0 Software para gestion de la planeación operativa.
+ * PHP v5
+ * Autor: Prof. Jesus Antonio Peyrano Luna <antonio.peyrano@live.com.mx>
+ * Nota aclaratoria: Este programa se distribuye bajo los terminos y disposiciones
+ * definidos en la GPL v3.0, debidamente incluidos en el repositorio original.
+ * Cualquier copia y/o redistribucion del presente, debe hacerse con una copia
+ * adjunta de la licencia en todo momento.
+ * Licencia: http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+
+    /*
+     *Este archivo contiene la declaraci�n de procedimientos para la creaci�n de la pagina INDEX.
+     */
+    header('Content-Type: text/html; charset=UTF-8'); //Forzar la codificación a UTF-8.     
+    include_once("./php/backend/index.class.php"); //Se carga el archivo de clase.
+    $index = new index(); //Se crea la instancia de objeto index.
+    $index->drawUI(); //Se ejecuta la funci�n para visualizar la UI.
+?>
